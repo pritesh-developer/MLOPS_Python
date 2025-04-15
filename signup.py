3485a07db8fda279350312e5a1a0da730e9c4b16
Pritesh Paul <priteshpaul@priteshs-air.home>
2025-04-15 21:10:14 +0100

    second commit

---
+++ signup.py
@@ -0,0 +1,6 @@
+class signup:
+    def __init_(self):
+        self.username = ""
+        print("This is signup class")
+    def validate(self):
+        print("This is validate method")
--- user.py
+++ user.py
@@ -1,10 +1,21 @@
+from signup import *
+
 class user:
     def __init_(self):
         self.id=0
+        print("this is init")
+
+    def menu(self):
+        user_input = input("What do you want to do? (1: Signup , 2: Login)")
+        print(type(user_input))
+        if user_input == "1":
+            print("you have selected signup")
+            sup = signup()
+            sup.validate()
 
     def userDetails(self):
         print("This is user details")
 
 
 obj = user()
-obj.userDetails()
+obj.menu()
